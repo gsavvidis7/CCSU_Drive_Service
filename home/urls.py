abcab4a54f7b694804369e48cbc4beaa7458db59
gsavvidis7 <gsavvidis777@gmail.com>
2019-11-27 14:31:59 -0500

    did login

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('index/', views.index, name='index'),
     path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -17,6 +17,7 @@ def register(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
+        email = request.POST['email']
         username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
@@ -26,8 +27,12 @@ def register(request):
                 print("Error: Username already taken")
                 messages.info(request, 'Error: Username already taken')
                 return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                print("Error: Email already registered")
+                messages.info(request, 'Error: Email already registered')
+                return redirect('register')
             else:
-                User.objects.create_user(first_name=first_name, last_name=last_name, username=username,
+                User.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username,
                                          password=password1)
                 print("Registration complete")
         else:
@@ -40,3 +45,21 @@ def register(request):
     else:
         return render(request, 'register.html')
 
+def login(request):
+    #this is the method that authenticates the user based on username and password,
+    #also renders the login page if the request method is not a post
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        # Show the the index page if the user is authenticated, else give feedback saying invalid credentials
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
