8f730ccf7da5f0349b4ea3c78ce1beed9f671376
gsavvidis7 <gsavvidis777@gmail.com>
2019-12-02 20:26:07 -0500

    added ajax

---
+++ static/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    var ajaxForm = $(".form_ajax")
+    ajaxForm.submit(function(event){
+        event.preventDefault();
+        console.log("form is not sending");
+        var thisForm =$(this);
+        var actionEndpoint = thisForm.attr("action")
+        var httpMethod = thisForm.attr("method") ;
+        var formData = thisForm.serialize();
+
+        $.ajax({
+            url: actionEndpoint,
+            method: httpMethod,
+            data: formData,
+            success: function(){
+                alert("Data Saved")
+
+            }
+        })
+    })
+})
